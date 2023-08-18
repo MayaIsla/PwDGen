7f85a891e2c6289c5fc26a989d88ed8d1d0e635c
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2023-08-18 11:53:31 -0500

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -1,6 +1,19 @@
 import string    
 import random 
-S = 10   
- 
-pwd = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation , k = S))    
-print(str(pwd)) 
+N = 4   
+L = 4
+P = 1
+A = 1
+
+alpha = ''.join(random.choices( string.ascii_uppercase, k=A))
+lower = ''.join(random.choices( string.ascii_lowercase, k=L))
+num = ''.join(random.choices(string.digits, k=N))
+punc = ''.join(random.choices(string.punctuation, k=P))
+
+print(alpha + lower + num + punc)
+
+
+
+
+#pwd = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation , k = S))    
+#print(str(pwd)) 
